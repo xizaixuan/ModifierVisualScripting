428acafe7c5a3e0bedd6df80160c206987d75021
xizaixuan <285963400@qq.com>
2020-06-24 16:59:30 +0800

    modified

--- Assets/VisualScripting/Editor/Elements/UIHelper.cs
+++ Assets/VisualScripting/Editor/Elements/UIHelper.cs
@@ -2,7 +2,7 @@ namespace Modifier.Elements
 {
     public static class UIHelper
     {
-        public const string PackagePath = "VisualScripting/";
+        public const string PackagePath = "Assets/VisualScripting/";
         const string k_AssetPath = PackagePath + "Editor/";
         public const string TemplatePath = k_AssetPath + "Elements/Templates/";
         public const string DocumentationPath = PackagePath + "Documentation~/";
